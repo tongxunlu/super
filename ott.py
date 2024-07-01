513cb8183016e91bed69c95a8fc11d678979360c
tongxunlu <1849141+tongxunlu@users.noreply.github.com>
2024-07-01 14:41:39 +0800

    Update ott.py

--- ott.py
+++ ott.py
@@ -181,11 +181,11 @@ with open('mytvsuper.m3u', 'w', encoding='utf-8') as m3u_file:
     #url_tvg = "http://zzqwe.giize.com:12228/epgmysuper.xml,http://zzqwe.giize.com:12228/epganywhere.xml,http://epg.51zmt.top:8000/e.xml"
     catchup = "append"
     catchup_source = "?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"
-
+    catchup_days   ="7"
     #m3u_file.write('#EXTM3U url-tvg=\"{url_tvg}\" catchup="{catchup}" catchup-source="{catchup_source}"\n')
     #m3u_file.write("#EXTM3U url-tvg=\"https://cfpgwztz.wofuck.rr.nu/wztz/http/xmltv.bph.workers.dev\"\n")
     #m3u_file.write(f'#EXTM3U url-tvg=\"http://epg.51zmt.top:8000/e.xml\" catchup=\"{catchup}\" catchup-source=\"{catchup_source}\"\n')
-    m3u_file.write(f'#EXTM3U url-tvg=\"https://cfpgwztz.wofuck.rr.nu/wztz/http/xmltv.bph.workers.dev\" catchup=\"{catchup}\" catchup-source=\"{catchup_source}\"\n')
+    m3u_file.write(f'#EXTM3U url-tvg=\"https://cfpgwztz.wofuck.rr.nu/wztz/http/xmltv.bph.workers.dev\" catchup-days=\"{catchup_days}\"  catchup=\"{catchup}\" catchup-source=\"{catchup_source}\"\n')
     # 遍历所有频道并写入每个频道的 M3U 内容
     for channel_code in CHANNEL_LIST.keys():
         m3u_content = get_mytvsuper(channel_code)
